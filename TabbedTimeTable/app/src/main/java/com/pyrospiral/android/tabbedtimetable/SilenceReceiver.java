b4a09a35485050b66abb167d10b45b31364b54e7
pyrospiral <chauhankushagra1@gmail.com>
2015-02-07 02:39:02 +0530

    silence fixed

--- TabbedTimeTable/app/src/main/java/com/pyrospiral/android/tabbedtimetable/SilenceReceiver.java
+++ TabbedTimeTable/app/src/main/java/com/pyrospiral/android/tabbedtimetable/SilenceReceiver.java
@@ -1,51 +1,70 @@
 package com.pyrospiral.android.tabbedtimetable;
 
-import android.app.AlarmManager;
-import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.preference.PreferenceManager;
+import android.media.AudioManager;
 import android.util.Log;
 import android.widget.Toast;
-
-import java.util.Calendar;
 
 /**
  * Created by Kush on 2/6/2015.
  */
 public class SilenceReceiver extends BroadcastReceiver {
+
+    private AudioManager mAudioManager;
+    private Intent mIntent;
+    private int val;
+
+
     @Override
     public void onReceive(Context context, Intent intent) {
+
+        SharedPreferences sharedPreferences = context.getSharedPreferences("alarmPref", Context.MODE_PRIVATE);
+        SharedPreferences.Editor edit = sharedPreferences.edit();
+
         Log.e("Receiver","received");
         Toast.makeText(context, "Alarm !!!!!!!!!!", Toast.LENGTH_LONG).show();
 
+        mIntent = intent;
+
+        if(mIntent != null)
+        {
+
+            val = mIntent.getIntExtra("value",5);
+            Log.e("Silence service","mIntent  "+val);
+        }
+
+        mAudioManager = (AudioManager)context.getSystemService(Context.AUDIO_SERVICE);
+
+
+
+
+
+
+        if(val == 1 && sharedPreferences.getInt("silent",0)==0) {
+            Log.e("Silence service","phone silent");
+            Toast.makeText(context, "Phone on silent", Toast.LENGTH_SHORT).show();
+            mAudioManager.setRingerMode(AudioManager.RINGER_MODE_SILENT);
+            edit.putInt("silent", 1);
+            edit.putInt("normal", 0);
+            edit.apply();
+
+        }
+
+
+        if(val == 2 && sharedPreferences.getInt("normal",0)==0) {
+            Log.e("Silence service","phone normal");
+            Toast.makeText(context, "Phone removed from silent", Toast.LENGTH_SHORT).show();
+            mAudioManager.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
+            edit.putInt("normal", 1);
+            edit.putInt("silent", 0);
+            edit.apply();
+
+        }
+
     }
 
-    public void SetAlarm(Context context)
-    {
-
-        Intent i = new Intent(context, SilenceReceiver.class);
-        PendingIntent pi = PendingIntent.getBroadcast(context, 5, i, 0);
-
-        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
-        boolean value = prefs.getBoolean("silent_checkbox", true);
-
-
-
-            Calendar cal = Calendar.getInstance();
-            //Get time from database
-            cal.set(Calendar.HOUR_OF_DAY, 21);
-            cal.set(Calendar.MINUTE, 23);
-            cal.set(Calendar.SECOND, 0);
-
-
-            AlarmManager am2 = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
-            am2.set(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(), pi);
-
-
-
-    }
 
 }
